04785bcc8c4f4cd1036d545cc4583d41f245dc4a
jianbai.gbj <jianbai.gbj@alibaba-inc.com>
2017-11-28 15:55:50 +0800

    [WEEX-130][android]WSON High Performance Binary Data Format Supported use json default

--- android/sdk/src/main/java/com/taobao/weex/bridge/WXBridgeManager.java
+++ android/sdk/src/main/java/com/taobao/weex/bridge/WXBridgeManager.java
@@ -1735,7 +1735,7 @@ public class WXBridgeManager implements Callback, BactchExecutor {
 
       WXJSObject[] args = {
               new WXJSObject(WXJSObject.String, instanceId),
-              new WXJSObject(WXJSObject.JSON, WXJsonUtils.fromObjectToJSONString(tasks))};
+              new WXJSObject(WXJSObject.WSON, Wson.toWson(tasks))};
       invokeExecJS(String.valueOf(instanceId), null, METHOD_CALL_JS, args);
     } catch (Throwable e) {
       WXLogUtils.e("WXBridgeManager", e);
